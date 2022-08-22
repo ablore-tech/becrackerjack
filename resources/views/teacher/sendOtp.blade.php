5dfefecff738c7ae25afb1df256fc2004c3187ba
waqar ansari <waqar.78692@yahoo.com>
2022-08-23 00:42:26 +0530

    signin

--- resources/views/teacher/sendOtp.blade.php
+++ resources/views/teacher/sendOtp.blade.php
@@ -36,7 +36,7 @@
 
                         <div class="card-body p-4">
                             <div class="text-center mt-2">
-                                <h5 class="text-primary">Welcome Back !</h5>
+                                <h5 class="text-primary">Welcome Back!</h5>
                                 <p class="text-muted">Sign in to continue to BCJ.</p>
                             </div>
                             <div class="p-2 mt-4">
